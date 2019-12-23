60e6ac840f2d8c86c6bd377e71b8a348504231f3
richasyal <richasyal@gmail.com>
2019-12-23 11:53:59 +0800

    more tweaks to timeline

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 
 
-    $(".regular").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
+    // $(".regular").slick({
+    //   dots: true,
+    //   infinite: true,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 3
+    // });
 		
+
+		$(function(){
+   $('.timeline-nav').slick({
+      slidesToShow: 12,
+      slidesToScroll: 1,
+      asNavFor: '.timeline-slider',
+      centerMode: false,
+      focusOnSelect: true,
+       mobileFirst: true,
+      arrows: false,
+      infinite:true,
+       responsive: [
+           {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 8,
+           }
+          },
+         {
+          breakpoint: 0,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2,
+          }
+        }
+     ]
+  });
+  
+   $('.timeline-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      asNavFor: '.timeline-nav',     
+      centerMode: true,     
+      cssEase: 'ease',
+       edgeFriction: 0.5,
+       mobileFirst: true,
+       speed: 500,
+       responsive: [
+         {
+          breakpoint: 0,
+          settings: {
+              centerMode: false
+          }
+        },
+           {
+          breakpoint: 768,
+          settings: {
+              centerMode: true
+          }
+        }
+     ]
+  });
+ 
+});
